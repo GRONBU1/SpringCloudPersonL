e4421570806400b71f1f45086bad4137a05ee502
GRONBU1 <1174650302@qq.com>
2019-07-16 22:02:28 +0800

    .

--- feign/src/main/java/com/springcloudl/feign/feign/SchedualServiceHi.java
+++ feign/src/main/java/com/springcloudl/feign/feign/SchedualServiceHi.java
@@ -1,11 +1,12 @@
 package com.springcloudl.feign.feign;
 
+import com.springcloudl.feign.hystrix.SchedualServiceHiHystric;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "eurka-client")
+@FeignClient(value = "eurka-client",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
